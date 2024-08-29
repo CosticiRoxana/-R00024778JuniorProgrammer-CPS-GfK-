402f1ac0710c06425f910dc28e074e011417868c
CosticiRoxana <costiciroxana@gmail.com>
2024-08-29 19:11:52 +0300

    Describe the changes

--- costiciRoxana/views.py
+++ costiciRoxana/views.py
@@ -1,43 +1,56 @@
 import requests
 from django.shortcuts import render
+from django.db import DatabaseError
 from .models import WeatherForecast
-from datetime import datetime
+import datetime
 
-# task1.1
 def get_weather(request):
     weather_data = None
     city_name = None
+    error_message = None  
 
     if request.method == 'POST':
         city_name = request.POST['city_name']
         api_key = '7f8db97ba1074ce08aa120837242908'  
         url = f"http://api.weatherapi.com/v1/forecast.json?key={api_key}&q={city_name}&days=3"
 
-        response = requests.get(url)
-        if response.status_code == 200:
+        try:
+            response = requests.get(url)
+            response.raise_for_status()  
+
             data = response.json()
             weather_data = data['forecast']['forecastday']
-#end task1.1
 
-#task1.2
             for day in weather_data:
-                date = datetime.strptime(day['date'], '%Y-%m-%d').date()
+                date = datetime.datetime.strptime(day['date'], '%Y-%m-%d').date()
                 max_temp = day['day']['maxtemp_c']
                 min_temp = day['day']['mintemp_c']
                 total_precipitation = day['day']['totalprecip_mm']
-                sunrise_hour = datetime.strptime(day['astro']['sunrise'], '%I:%M %p').time()
-                sunset_hour = datetime.strptime(day['astro']['sunset'], '%I:%M %p').time()
+                sunrise_hour = datetime.datetime.strptime(day['astro']['sunrise'], '%I:%M %p').time()
+                sunset_hour = datetime.datetime.strptime(day['astro']['sunset'], '%I:%M %p').time()
 
-                WeatherForecast.objects.update_or_create(
-                    date=date,
-                    city=city_name,
-                    defaults={
-                        'max_temp': max_temp,
-                        'min_temp': min_temp,
-                        'total_precipitation': total_precipitation,
-                        'sunrise_hour': sunrise_hour,
-                        'sunset_hour': sunset_hour
-                    }
-                )
-# end task1.2
-    return render(request, 'index.html', {'weather_data': weather_data, 'city_name': city_name})
+                try:
+                    WeatherForecast.objects.update_or_create(
+                        date=date,
+                        city=city_name,
+                        defaults={
+                            'max_temp': max_temp,
+                            'min_temp': min_temp,
+                            'total_precipitation': total_precipitation,
+                            'sunrise_hour': sunrise_hour,
+                            'sunset_hour': sunset_hour
+                        }
+                    )
+                except DatabaseError as e:
+                    error_message = "A apărut o eroare la salvarea datelor în baza de date."
+                    print(f"Eroare baza de date: {e}") 
+
+        except requests.exceptions.RequestException as e:
+            error_message = "Nu am putut obține datele meteo. Verifică conexiunea la internet sau orașul introdus."
+            print(f"Eroare cerere API: {e}")  
+
+    return render(request, 'index.html', {
+        'weather_data': weather_data,
+        'city_name': city_name,
+        'error_message': error_message
+    })
